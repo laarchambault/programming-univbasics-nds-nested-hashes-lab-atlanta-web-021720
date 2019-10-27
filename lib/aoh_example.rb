9f77f264e0c5caba0898d48ee87824197397f02b
Laura Archambault <laarchambault@gmail.com>
2019-10-27 20:54:26 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -23,14 +23,20 @@ def literal_aoh
   # held in the constants
   arr = [
     { :name => "Don Gatley", :occupation => "Live-in Staff" }
-    { :name => "Don Gatley", :occupation => "Live-in Staff" }
+    { :name => "Joelle van Dyne", :occupation => "Radio Personality" }
+    { :name => "Pat Monteseian", :occupation => "Staff" }
+    { :name => "Kate Gompert", :occupation => "None" }
+    { :name => "Bruce Green", :occupation => "Fan of Mildred" }
     ]
 end
 
 def aoh_lookup(aoh, row, key)
+  return aoh[row][key]
 end
 
 def aoh_update(aoh, row, key, new_value)
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
+  aoh[row][key] = new_value
+  return aoh
 end
